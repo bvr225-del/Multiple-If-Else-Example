0f4a4f73beefe97cd98c285b9fdd1eec49493d2c
dell <dell@DESKTOP-S8CP3CH>
2026-01-02 11:02:17 +0530

    Add project files.

---
+++ Multiple If Else Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_If_Else_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
